8500673e2cf236272305e83cb9b5a9a42807ad7c
Markus Kauschmann <markus.kauschmann@stud.uni-hannover.de>
2024-01-16 20:52:52 +0100

    Ct first Version

--- src/components/ControlTable.vue
+++ src/components/ControlTable.vue
@@ -1,46 +1,77 @@
 <template>
   <!-- Schaltfläche zum Hinzufügen einer neuen Reihe -->
   <v-btn @click="controlTable.addRow">Neue Reihe hinzufügen</v-btn>
-
-  <v-data-table
-    :headers="headers"
+  <v-btn @click="controlTable.showTableConsole">Show Table</v-btn>
+  <v-divider></v-divider>
+  <v-table
     :items="controlTable.controlTable"
     density="compact"
-    height="85vh"
+    height="91vh"
   >
-  
-  </v-data-table>
+  <thead>
+      <tr>
+        <th class="center">Breakpoint</th>
+        <th class="center">Label</th>
+        <th class="center">Adress</th>
+        <th class="center">AluSelA</th>
+        <th class="center">AluSelB</th>
+        <th class="center">MDRSel</th>
+        <th class="center">HsCs</th>
+        <th class="center">Hs_R_W</th>
+        <th class="center">AluCtrl</th>
+        <th v-for="r in registerStore.registerOrder" class="center">{{r}}</th>
+        <th class="center">ALU == 0?</th>
+        <th class="center">next</th>
+        <th class="center">description</th>
+        <th class="center">Aktionen</th>
+      </tr>
+    </thead>
+    <draggable :list="list" tag="tbody" item-key="id" group="signalTable"  @change="controlTable.updateTable">
+      <template #item="{element, index}">
+        <tr>
+          <td @click="element.breakpoint = !element.breakpoint">{{ element.breakpoint }}</td>
+          <td>{{ element.label }}</td>
+          <td>{{ element.adress }}</td>
+          <td>
+            <v-select :hide-details="true" density="compact" variant="outlined" menu-icon="" :items="multiplexerStore.muxA" v-model="element.AluSelA">
+            </v-select>
+          </td>
+          <td>
+            <v-select :hide-details="true" density="compact" variant="outlined" menu-icon="" :items="multiplexerStore.muxB" v-model="element.AluSelB">
+            </v-select>
+          </td>
+          <td @click="element.MDRSel = !element.MDRSel">{{ +element.MDRSel }}</td>
+          <td @click="element.HsCs = !element.HsCs">{{ +element.HsCs }}</td>
+          <td @click="element.Hs_R_W = !element.Hs_R_W">{{ +element.Hs_R_W }}</td>
+          <td>
+            <v-select :hide-details="true" density="compact" variant="outlined" menu-icon="" :items="aluStore.aluOperationsListAdded" v-model="element.AluCtrl">
+            </v-select>
+          </td>
+          <td v-for="(value, key) in element.registerWrite" :key="key" class="center" @click="element.registerWrite[key] = element.registerWrite[key] === 0 ? 1 : 0">
+            {{ value }}
+          </td>
+          <td>{{ element.jump }}</td>
+          <td>{{ element.next }}</td>
+          <td>{{ element.description }}</td>
+          <td><v-btn @click="controlTable.deleteRow(index)">Löschen</v-btn></td>
+        </tr>
+    </template>
+  </draggable>
+</v-table>
 </template>
 
 <script setup lang="ts">
 import { useControlTableStore } from '@/store/ControlTableStore';
 import { useRegisterStore } from '@/store/RegisterStore';
+import { useMultiplexerStore } from '@/store/MultiplexerStore';
+import { useAluStore } from '@/store/AluStore';
+import draggable from 'vuedraggable';
 
 const registerStore = useRegisterStore();
 const controlTable = useControlTableStore();
+const multiplexerStore = useMultiplexerStore();
+const aluStore = useAluStore();
 
-// Erzeugen der Spalten-Header
-const headers = [
-  // Header für die 'breakpoint' und 'label' Spalten
-  {title: 'Breakpoint', value: 'breakpoint' },
-  {title: 'Label', value: 'label' },
-  {title: 'Adress', value: 'adress'},
-  {title: 'AluSelA', value: 'AluSelA'},
-  {title: 'AluSelB', value: 'AluSelB'},
-  {title: 'MDRSel', value: 'MDRSel'},
-  {title: 'HsCs', value: 'HsCs'},
-  {title: 'Hs_R_W', value: 'Hs_R_W'},
-  {title: 'AluCtrl', value: 'AluCtrl'},
-    // Dynamisch generierte Header für die Register
-    ...registerStore.registerOrder.map(register => ({
-    title: register, // Der Name des Registers
-    value: `registerWrite.${register}` // Der Schlüssel für den Zugriff auf den Wert im Objekt
-  })),
-  {title: 'jump', value: 'jump'},
-  {title: 'next', value: 'next'},
-  {title: 'description', value: 'description'},
-];
-
-headers.push({ title: 'Aktionen', value: 'actions' });
+const list = controlTable.controlTable;
 
 </script>
--- src/store/ControlTableStore.ts
+++ src/store/ControlTableStore.ts
@@ -4,17 +4,19 @@ import { useAluStore } from '@/store/AluStore';
 import { useRegisterStore } from '@/store/RegisterStore';
 import { useMultiplexerStore } from '@/store/MultiplexerStore';
 import { useMemoryStore } from './MemoryStore';
+import { v4 as uuidv4 } from 'uuid'
 
 // Interface for control table
 interface ControlTable {
+  id: string;
   breakpoint: boolean;
   label: string;
   adress: number;
   AluSelA: any;
   AluSelB: any;
-  MDRSel: number;
-  HsCs: number;
-  Hs_R_W: number;
+  MDRSel: boolean;
+  HsCs: boolean;
+  Hs_R_W: boolean;
   AluCtrl: any;
   registerWrite: { [key: string]: number }; // Für jedes Register ein Boolescher Wert
   jump: number;
@@ -33,37 +35,57 @@ export const useControlTableStore = defineStore('controlTable', () => {
 
   function addRow(){
     const newRow: ControlTable = {
+      id: uuidv4(),
       breakpoint: false,
       label: "",
-      adress: 0,
+      adress: controlTable.length,
       AluSelA: null,
       AluSelB: null,
-      MDRSel: 0,
-      HsCs: 0,
-      Hs_R_W: 0,
+      MDRSel: false,
+      HsCs: false,
+      Hs_R_W: false,
       AluCtrl: null,
       registerWrite: {},
       jump: -1,
-      next: 0,
+      next: controlTable.length + 1,
       description: "",
     };
   
     // Für jedes Register einen Standardwert hinzufügen
-    registerStore.registerOrder.forEach(register => {
+    registerStore.getRegister.value.forEach(register => {
       newRow.registerWrite[register] = 0;
     });
+    console.log(newRow.registerWrite);
   
     controlTable.push(newRow);
+    updateAdressesAndNext();
+  }
+
+  function showTableConsole(){
     console.log(controlTable);
   }
 
+  function updateAdressesAndNext() {
+    controlTable.forEach((row, index) => {
+      row.adress = index; // Setze die Adresse auf den aktuellen Index
+      row.next = index + 1 < controlTable.length ? index + 1 : -1; // Setze 'next' auf den nächsten Index oder -1, wenn es das letzte Element ist
+    });
+  }
+
+  function updateTable(){
+    updateAdressesAndNext()
+  }
+  
   function deleteRow(index: number){
     controlTable.splice(index, 1);
+    updateAdressesAndNext(); // Aktualisiere Adressen und Next-Werte nach dem Löschen
   }
 
   return {
     controlTable,
     addRow,
-    deleteRow
+    deleteRow,
+    showTableConsole,
+    updateTable
   };
 });

daa64696fa04b888f36829903b0ae6960093fcf5
Markus Kauschmann <markus.kauschmann@stud.uni-hannover.de>
2024-04-11 12:07:44 +0200

    Refactor RegisterView component: Add detail view for editing register values

--- src/components/ReUsable/RegisterView.vue
+++ src/components/ReUsable/RegisterView.vue
@@ -34,22 +34,47 @@
                     <td class=" pl-0 text-center pr-0">{{ data.Value }}</td>
                     <td class=" pl-0 text-center pr-0">0x{{ toHex(data.Value) }}</td>
                     <td width="10vw">
-                        <v-icon @click="changeValue">mdi-pencil</v-icon>
+                        <v-icon @click="changeValue(data.title, data.Value)">mdi-pencil</v-icon>
                     </td>
                 </tr>
             </tbody>
             </v-table>
         </v-card-text>
     </v-card>
+    <v-dialog v-model="detailViewVisible" persistent max-width="23vw" min-width="500px">
+       <v-card>
+            <v-card-title>
+            Register
+            </v-card-title>
+            <v-card-text>
+                <Dec_Hex_Bin_Inputs
+                    :numberInput="Number(numberInput)"
+                    @update:dec="numberInput = $event"
+                    @inputCleared="handleInputCleared"
+                />
+            </v-card-text>
+            <v-card-actions>
+                <v-spacer>
+                    <v-btn color="primary" @click="cancel">{{ $t('button.cancel') }}</v-btn>
+                    <v-btn color="primary" @click="updateItem">{{ $t('button.save') }}</v-btn>
+                </v-spacer>
+            </v-card-actions>
+        </v-card>
+    </v-dialog>
 </template>
 
 <script setup lang="ts">
 import { useRegisterStore } from '@/store/RegisterStore';
 import Dec_Hex_Bin_Inputs from './Dec_Hex_Bin_Inputs.vue';
+import { ref } from 'vue';
 
 const registerStore = useRegisterStore();
+const numberInput = ref(0);
+const detailViewVisible = ref(false);
+const currentRegister = ref("");
+const currentRegisterValue = ref(0);
 
-function toHex(value): string {
+function toHex(value:any): string {
   if (value < 0) {
     value = 0xFFFFFFFF + value + 1; // Umwandlung in das Zweierkomplement
   }
@@ -58,11 +83,30 @@ function toHex(value): string {
   while (hex.length < 6) { // Stellen sicher, dass die Länge der Zeichenfolge 8 beträgt
       hex = '0' + hex;
   }
-  
   return hex; // Rückgabe mit dem "0x" Präfix
 }
 
-function changeValue() {
-  console.log('changeValue');
+const handleInputCleared = () => {
+  numberInput.value = 0;
+};
+
+function cancel() {
+    detailViewVisible.value = false;
+    handleInputCleared();
+}
+
+function updateItem() {
+    detailViewVisible.value = false;
+    if(numberInput.value == null || numberInput.value == undefined) {
+        numberInput.value = 0;
+    }
+    registerStore.updateRegisterData(currentRegister.value, numberInput.value, registerStore.getRegisterDescription(currentRegister.value));
+}
+
+function changeValue(data:any, value:any) {
+    detailViewVisible.value = true;
+    currentRegister.value = data;
+    numberInput.value = value;
+    currentRegisterValue.value = value;
 }
 </script>

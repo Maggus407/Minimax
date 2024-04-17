019d39fbd169a335bae1a2f3da00f172f4796a69
Markus Kauschmann <markus.kauschmann@stud.uni-hannover.de>
2024-04-17 20:49:18 +0200

    new bib for CT - in production

--- src/components/ControlTable.vue
+++ src/components/ControlTable.vue
@@ -32,28 +32,181 @@
   </div>
 
   <v-divider></v-divider>
-    <VirtualList
-      v-model="table"
-      :dataKey="'id'"
-      :handle="'#drag'"
-      style="height: 92vh;"
-      @drop="handleDrop"
-    >
-      <template #item="{ record, index, dataKey }">
-        <p>{{ record }}</p>
-        <v-icon id="drag" class="drag">mdi-drag-horizontal-variant</v-icon>
-        <p @click.stop="record.breakpoint = !record.breakpoint">
+  <VirtualList
+    v-model="table"
+    :dataKey="'id'"
+    :handle="'#drag'"
+    style="height: 92vh;"
+    @drop="handleDrop"
+    rootTag="div" 
+    wrapTag="div" 
+    itemTag="div"  
+    class="virtual-table"
+  >
+    <template #header>
+      <div class="virtual-table-header">
+        <div v-for="header in fullHeaders" :key="header.key" class="virtual-table-header-cell">
+          {{ header.label }}
+        </div>
+      </div>
+    </template>
+    <template #item="{ record, index, dataKey }">
+      <div class="virtual-table-row">
+        <!--Breakpoint-->
+        <div class="virtual-table-cell" width="100vw" @click.stop="record.breakpoint = !record.breakpoint">
           <v-icon v-if="record.breakpoint" color="red">mdi-record</v-icon>
           <p v-else>&nbsp;</p>
-        </p>
-        <p>{{ record.adress = index }}</p>
-      </template>
-      <template v-slot:header>
-        <div class="d-flex flex-row">
-          <p class="pa-3" v-for="header in fullHeaders">{{ header }}</p>
         </div>
-      </template>
-    </VirtualList>
+        <!--Label-->
+        <div class="virtual-table-cell">
+          <v-text-field @change="controlTable.updateTable()" v-model="record.label" dense solo-inverted hide-details></v-text-field>
+        </div>
+        <!--Adress-->
+        <div class="virtual-table-cell">
+          {{ record.adress = index }}
+        </div>
+        <div class="virtual-table-cell" @click="update(record, record.registerA)">
+          <v-select
+              :hide-details="true"
+              density="compact"
+              variant="outlined"
+              menu-icon=""
+              :items="['-', ...multiplexerStore.muxA]"
+              v-model="record.AluSelA"
+              return-object
+              @update:modelValue="update(record, null)"
+            >
+          </v-select>
+        </div>
+        <div class="virtual-table-cell" @click="update(record, record.registerB)">
+          <v-select
+              :hide-details="true"
+              density="compact"
+              variant="outlined"
+              menu-icon=""
+              :items="['-', ...multiplexerStore.muxB]"
+              v-model="record.AluSelB"
+              return-object
+              @update:modelValue="update(record, null)"
+            >
+          </v-select>
+        </div>
+        <!--MDRSel-->
+        <div class="virtual-table-cell cursor-pointer" @click.stop="update(record,null,null,'MDRSel')">{{ +record.MDRSel }}</div>
+        <!--HsCs-->
+        <div class="virtual-table-cell cursor-pointer" @click.stop="update(record,null,null,'HsCs')">{{ +record.HsCs }}</div>
+        <!--Hs_R_W-->
+        <div class="virtual-table-cell cursor-pointer" @click.stop="update(record,null,null,'Hs_R_W')">{{ +record.Hs_R_W }}</div>
+        <!--ALU-->
+        <div class="virtual-table-cell">
+          <v-select
+              :hide-details="true"
+              density="compact"
+              menu-icon=""
+              variant="outlined"
+              :items="['-', ...aluStore.aluOperationsListAdded]"
+              v-model="record.AluCtrl"
+              @update:modelValue="update(record, null)"
+            >
+            </v-select>
+        </div>
+        <!--Register-->
+        <div v-for="register in record.registerWrite" :key="register.title" class=" virtual-table-cell center pointer pr-0 pl-0 text-center" @click.stop="update(record, register)">
+                <p>{{ register.isActive ? 1 : 0 }}</p>
+        </div>
+        <!--ALU == 0?-->
+        <div class="virtual-table-cell" @click.stop="openDialog(record)">
+            <p v-if="record.jump === null">{{ record.jump !== null ?  record.jump.adress : "-"}}</p>
+            <div class="flex flex-col">
+              <p v-if="record.jump !== null && record.jumpSet === true">1</p>
+              <p v-if="record.jump !== null && record.jumpSet === true">0</p>
+            </div>
+          </div>
+          <!--Next-->
+          <div class="virtual-table-cell pr-0 pl-0 text-center">
+            <div class="flex flex-col">
+              <p v-if="record.jump !== null">{{ record.jump.adress }}</p>
+              <p v-if="true">{{ typeof record.next === 'object' && record.next !== null ? record.next.adress : record.next }}</p>
+            </div>
+          </div>
+           <!-- description -->
+           <div class="virtual-table-cell pr-0 pl-2">
+            <div v-if="record.description.length > 0">
+              <p v-for="(d, index) in record.description" :key="index">{{ d }}</p>
+            </div>
+            <div v-else>
+              <p>&nbsp;</p>
+            </div>
+          </div>
+          <!-- Aktionen -->
+          <div class=" virtual-table-cell pr-0 pl-0">
+            <div class="d-flex flex-row justify-end">
+              <v-tooltip text="Write Comment">
+                <template v-slot:activator="{ props }">
+                  <v-icon v-bind="props" class="mr-5">mdi-text-box-edit</v-icon>
+                </template>
+              </v-tooltip>
+
+              <v-icon @click.stop="controlTable.deleteRow(index)" color="red">mdi-delete</v-icon>
+            </div>
+          </div>
+          <i id="drag" class="drag">drag me</i>
+      </div>
+    </template>
+  </VirtualList>
+  <v-dialog v-model="dialog" persistent max-width="30vw">
+    <v-card>
+      <v-card-title>
+        Sprung-Einstellungen
+      </v-card-title>
+      <v-card-text>
+        <v-radio-group v-model="selectedJumpType">
+          <v-radio label="Nächster Befehl" value="next"></v-radio>
+          <v-radio label="Unbedingter Sprung" value="unconditional"></v-radio>
+          <v-radio label="Bedingter Sprung" value="conditional"></v-radio>
+        </v-radio-group>
+        <div v-if="selectedJumpType === 'next'" ></div>
+          <div v-if="selectedJumpType === 'unconditional'">
+            <v-text-field
+              label="Unbedingter Sprung"
+              type="number"
+              v-model="unconditionalJump"
+              :max="controlTable.controlTable.length - 1"
+              :min="0"
+            ></v-text-field>
+        </div>
+        <div v-if="selectedJumpType === 'conditional'">
+          <v-text-field
+            label="ALU != 0?"
+            type="number"
+            v-model="conditionalJumpIfNotZero"
+            :max="controlTable.controlTable.length - 1"
+            :min="0"
+            :rules="[requiredRule]"
+          ></v-text-field>
+          <v-text-field
+            label="ALU == 0?"
+            type="number"
+            v-model="conditionalJumpIfZero"
+            :max="controlTable.controlTable.length - 1"
+            :min="0"
+            :rules="[requiredRule]"
+          ></v-text-field>
+        </div>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer></v-spacer>
+        <v-btn color="blue darken-1" @click="closeDialog">Abbrechen</v-btn>
+        <v-btn
+            color="green darken-1"
+            @click="applyJumpSettings"
+            :disabled="isOkButtonDisabled"
+          >
+            OK
+          </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script setup lang="ts">
@@ -86,7 +239,7 @@ const isCommentDialog = ref(false);
 
 // Definiere den Anfang des Headers bis zu "AluCtrl"
 const headers = [
-    "Breakpoint",
+    "Br",
     "Label",
     "Adress",
     "AluSelA",
@@ -101,21 +254,26 @@ const headers = [
 const trailingHeaders = [
     "ALU == 0?",
     "Next",
-    "RT-Notation"
+    "RT-Notation",
+    "Action"
 ];
 
 // Hole die Titel der Register aus dem 'registerStore'
 const registerTitles = registerStore.registerOrder.map((r:any) => r.title);
 
 // Füge die Register-Titel zwischen den festen Header-Teilen ein
-const fullHeaders = headers.concat(registerTitles).concat(trailingHeaders);
+const fullH = headers.concat(registerTitles).concat(trailingHeaders);
+const fullHeaders = fullH.map(header => ({
+    key: header,  // Use the header as the key
+    label: header  // Use the header as the label, too
+}));
 
 console.log(fullHeaders);
 
 const handleDrop = (event: any) => {
   console.log(event);
   console.log('change');
-  controlTable.updateAdressesAndNext();
+  controlTable.updateTable();
   console.log(controlTable.controlTable);
 };
 
@@ -213,14 +371,21 @@ function findRowById(adress: any) {
   return controlTable.getNextRowById(adress);
 }
 
-function addRowBetween() {
-
-}
-
 </script>
 
 <style scoped>
 .pointer{
   cursor: pointer;
+}
+.virtual-table-header, .virtual-table-row {
+  display: flex;
+}
+.virtual-table-header-cell, .virtual-table-cell {
+  
+  padding: 10px;
+  border: 1px solid #ccc;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 </style>

dd85e34522b69c189a552966a81e428151c88345
Markus Kauschmann <markus.kauschmann@stud.uni-hannover.de>
2024-06-10 11:29:44 +0200

    base

--- vite.config.ts
+++ vite.config.ts
@@ -9,6 +9,7 @@ import { fileURLToPath, URL } from 'node:url'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/HIER-UNTERVERZEICHNIS/',
   plugins: [
     vue({
       template: { transformAssetUrls},
